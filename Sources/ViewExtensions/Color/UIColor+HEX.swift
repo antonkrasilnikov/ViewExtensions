81b15c271cfa30ac77cfa90eac10a59e202289f0
Anton Krasilnikov <a.krasilnikov@incrdbl.me>
2024-03-27 09:48:20 -0300

    add color hex alpha initializers

--- Sources/ViewExtensions/Color/UIColor+HEX.swift
+++ Sources/ViewExtensions/Color/UIColor+HEX.swift
@@ -2,14 +2,14 @@ import UIKit
 
 public extension UIColor {
 
-    convenience init(hex: UInt) {
+    convenience init(hex: UInt, alpha: CGFloat = 1) {
         let red = CGFloat((hex & 0xFF0000) >> 16)/255.0
         let green = CGFloat((hex & 0x00FF00) >>  8)/255.0
         let blue = CGFloat((hex & 0x0000FF) >>  0)/255.0
-        self.init(red: red, green: green, blue: blue, alpha: 1)
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 
-    convenience init(hex: String) {
+    convenience init(hex: String, alpha: CGFloat = 1) {
         let scanner = Scanner(string: hex)
         scanner.charactersToBeSkipped = CharacterSet.alphanumerics.inverted
 
